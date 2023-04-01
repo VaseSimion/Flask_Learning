35581652071aabf78468f5e27c066ed318bf97df
VaseSimion <sularea.vasile@gmail.com>
2023-04-01 13:33:54 +0200

    box forms

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
+    images = []
+    for filename in os.listdir('static/Pictures'):
+        if filename.endswith('.jpg') or filename.endswith('.png'):
+            images.append(filename)
+    return render_template('home.html', photos=images[5:9])
 
 @app.route('/portfolio')
 def portfolio():
@@ -18,6 +18,12 @@ def portfolio():
         if filename.endswith('.jpg') or filename.endswith('.png'):
             images.append(filename)
     return render_template('portfolio.html', pictures=images)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
